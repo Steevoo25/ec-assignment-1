35d62198734845fb717707b3ed8e043db12d34f8
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-21 14:57:26 +0000

    Saved data as pickle file

--- data_processing.py
+++ data_processing.py
@@ -1,19 +1,22 @@
-
+from pickle import dump
 
 # --- Read File contents
 with open("att48.txt", "r") as file:
     file_contents = file.read()
+    
 # split by newline
 file_lines = file_contents.splitlines()
+
 # remove header info and EOF
 file_lines = file_lines[6:-1]
 
+# put into array
 us_capitals = []
-
 for capital in file_lines:
     # split each element
     _, x, y = capital.split()
     # put element into dictionary
     us_capitals.append((int(x),int(y)))
 
-print(us_capitals)
+with open("us_capitals.pkl", "wb") as file:
+    dump(us_capitals, file)
