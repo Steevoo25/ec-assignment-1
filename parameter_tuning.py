0444e9ee20541fb82f33454fabfe1b2320aa0063
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-22 14:36:48 +0000

    restructure

--- parameter_tuning.py
+++
@@ -1,6 +0,0 @@
-#from genetic_algorithm import ga
-from simulated_annealing import sa
-import matplotlib as plt
-
-# Test different parameter setting and collect the data (plot)
-print(sa(100,100,0.95))
