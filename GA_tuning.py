af31750f623afd0e6a3e7f27bd10e56a2ff290e0
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-29 12:25:48 +0000

    Added config options for tunig

--- GA_tuning.py
+++ GA_tuning.py
@@ -1,4 +1,12 @@
 from genetic_algorithm import ga
 import matplotlib as plt
 
-print(ga(100, 50, 1/50, 2, 6))
+iterations = 1000
+population_size = 50
+mutation_rate = 1 // population_size
+tournament_size = 2
+offspring_size = 6
+crossover_n = 5
+
+print(f"Result for iterations: {iterations}, population_size: {population_size}, mutation_rate: {mutation_rate}, tournament_size: {tournament_size}, offspring_size: {offspring_size}, crossover_n: {crossover_n}")
+print(ga(iterations, population_size, mutation_rate, tournament_size, offspring_size, crossover_n))
