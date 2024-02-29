d1581f0c255b201cb45fba3071c5aeb68dd9dbdb
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-29 13:44:19 +0000

    Added data processing and repeated runs

--- SA_tuning.py
+++ SA_tuning.py
@@ -1,5 +1,25 @@
 from simulated_annealing import sa
-import matplotlib as plt
-
+from matplotlib import pyplot as plt
+import pandas as pd
 # Test different parameter setting and collect the data (plot)
-print(sa(100,100,0.95))
+
+iterations = 100
+initial_temp = 100
+cooling_rate = 0.95
+
+trial_runs = list(range(10))
+print(sa(iterations, initial_temp, cooling_rate))
+columns = ["solution","fitness","iterations", "initial_temp", "cooling_rate"]
+
+df = pd.DataFrame(columns=columns)
+
+for i in trial_runs:
+    # edit parameters
+
+    # run SA
+    solution, fitness = sa(iterations, initial_temp, cooling_rate)
+    df.loc[i] = (solution, fitness, iterations, initial_temp, cooling_rate)
+    
+df.sort_values(by='fitness')
+df.index.name = 'Index'
+print(df)
