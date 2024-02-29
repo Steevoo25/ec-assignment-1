1717925494fa6b169ff51d4d3e23b544e71b85ed
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-02-29 15:17:51 +0000

    parameter tuning

--- GA_tuning.py
+++ GA_tuning.py
@@ -8,7 +8,7 @@ population_list = list(range(50, 550, 5))
 tournament_size = 2
 offspring_size = 20
 crossover_n = 5
-trial_runs = 1
+trial_runs = 5
 
 columns = ["solution","fitness","iterations", "population_size", "mutation_rate", "tournament_size", "offspring_size", "crossover_n" ]
 df = pd.DataFrame(columns=columns)
@@ -17,6 +17,7 @@ for i in range(trial_runs):
     # edit parameters
     population_size = population_list[i]
     mutation_rate = 1 // population_size
+
     # run GA
     solution, fitness = ga(iterations, population_size, mutation_rate, tournament_size, offspring_size, crossover_n)
     df.loc[i] = (solution, fitness, iterations, population_size, mutation_rate, tournament_size, offspring_size, crossover_n)
