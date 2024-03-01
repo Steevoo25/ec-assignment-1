c65c4980fd3f1338fda2ca7b3b98861b55865076
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-01 17:40:35 +0000

    cleanup + comments

--- SA_tuning.py
+++ SA_tuning.py
@@ -2,44 +2,33 @@ from simulated_annealing import sa
 from matplotlib import pyplot as plt
 import pandas as pd
 import optuna
+
 # Test different parameter setting and collect the data (plot)
-
-iteration_list = range(1,1001,5)
-initial_temp_list = range(50,150,5)
-cooling_rate_list = range(500, 750, 25)
-
-trial_runs = len(iteration_list)
 columns = ["solution","fitness","iterations", "initial_temp", "cooling_rate"]
 
+# Initialise df columns
 df = pd.DataFrame(columns=columns)
 
-# for i in range(trial_runs):
-#     # edit parameters
-#     iterations = iteration_list[i]
-#     initial_temp = initial_temp_list[i]
-#     cooling_rate = 0.95
-#     cooling_rate = cooling_rate_list[i] / 1000
-#     # run SA
-
-    
-
-
+# Returns fitness of SA with randomly generated parameters
 def objective(trial):
-
+    # Define parameter ranges
     iterations = trial.suggest_int('iterations', 10, 100)
     initial_temp = trial.suggest_int('initial_temp', 10, 50)
     cooling_rate = trial.suggest_float('cooling_rate', 0.5, 1)
-    
+    # Perform SA
     solution, fitness = sa(iterations, initial_temp, cooling_rate)
+    # Store results in df
     df.loc[trial.number] = (solution, fitness, iterations, initial_temp, cooling_rate)
-    
     return fitness
 
+# Tune parameters
 study = optuna.create_study()
 study.optimize(objective, n_trials=100)
 
+# Print best results
 print(study.best_params)
 
+# Show all results
 df = df.sort_values(by='fitness')
 df.index.name = 'Index'
 print(df)
