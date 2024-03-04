ec9b99c6db2da2c609e96037ad7cc4dde69001c0
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-04 19:53:20 +0000

    parameter ranges

--- GA_tuning.py
+++ GA_tuning.py
@@ -9,12 +9,12 @@ df = pd.DataFrame(columns=columns)
 
 def objective(trial):
     # Define parameter ranges
-    iterations = trial.suggest_int('iterations', 10, 100)
-    population_size = trial.suggest_int('population_size', 10, 100)
-    mutation_rate = 1 / iterations
-    tournament_size = trial.suggest_int('tournament_size', 2, 10)
-    offspring_size = trial.suggest_int('offspring_size', 3, 20)
-    penalty_weight = trial.suggest_int('penalty_weight', 1000, 5000 ) # higher means more penalty
+    iterations = 100
+    population_size = trial.suggest_int('population_size', 800, 1000)
+    mutation_rate = 10 / population_size
+    tournament_size = trial.suggest_int('tournament_size', 2, 6)
+    offspring_size = round(trial.suggest_float('offspring_size', 0.05, 0.1) * population_size)
+    penalty_weight = trial.suggest_int('penalty_weight', 2000, 10000 ) # higher means more penalty
     # Compute solution and fitness
     _, fitness = ga(iterations, population_size, mutation_rate, tournament_size, offspring_size, penalty_weight)
     df.loc[trial.number] = (fitness, iterations, population_size, mutation_rate, tournament_size, offspring_size, penalty_weight)
@@ -23,7 +23,7 @@ def objective(trial):
 # Create tuning study
 study = optuna.create_study()
 # Optimise objective over 100 runs
-study.optimize(objective, n_trials=100)
+study.optimize(objective, n_trials=50)
 
 # Print best results
 print(study.best_params)
