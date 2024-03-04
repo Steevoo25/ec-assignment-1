d62aff00d6136c823664848dd5e36ec7eece226b
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-04 17:13:06 +0000

    parameter ranges

--- SA_tuning.py
+++ SA_tuning.py
@@ -4,7 +4,7 @@ import pandas as pd
 import optuna
 
 # Test different parameter setting and collect the data (plot)
-columns = ["solution","fitness","iterations", "initial_temp", "cooling_rate"]
+columns = ["fitness","iterations", "initial_temp", "cooling_rate"]
 
 # Initialise df columns
 df = pd.DataFrame(columns=columns)
@@ -12,13 +12,13 @@ df = pd.DataFrame(columns=columns)
 # Returns fitness of SA with randomly generated parameters
 def objective(trial):
     # Define parameter ranges
-    iterations = trial.suggest_int('iterations', 10, 100)
-    initial_temp = trial.suggest_int('initial_temp', 10, 50)
-    cooling_rate = trial.suggest_float('cooling_rate', 0.5, 1)
+    iterations = trial.suggest_int('iterations', 1, 10)
+    initial_temp = trial.suggest_int('initial_temp', 800, 1200)
+    cooling_rate = trial.suggest_float('cooling_rate', 0.8, 0.99)
     # Perform SA
-    solution, fitness = sa(iterations, initial_temp, cooling_rate)
+    _, fitness = sa(iterations, initial_temp, cooling_rate)
     # Store results in df
-    df.loc[trial.number] = (solution, fitness, iterations, initial_temp, cooling_rate)
+    df.loc[trial.number] = (fitness, iterations, initial_temp, cooling_rate)
     return fitness
 
 # Tune parameters
@@ -28,7 +28,18 @@ study.optimize(objective, n_trials=100)
 # Print best results
 print(study.best_params)
 
+# Show whole dataframe
+pd.set_option('display.max_rows', None)
+
 # Show all results
 df = df.sort_values(by='fitness')
 df.index.name = 'Index'
 print(df)
+
+# filter by <59026
+df = df[df['fitness'] < 59026]
+# print mean and standard deviations
+print(df.mean())
+print(df.std())
+
+#observe results
