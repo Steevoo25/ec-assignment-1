24312b777bedd41d1aa434990e71c482dce03f3f
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-04 22:51:53 +0000

    whitespace

--- GA_tuning.py
+++ GA_tuning.py
@@ -14,7 +14,7 @@ def objective(trial):
     mutation_rate = 10 / population_size
     tournament_size = trial.suggest_int('tournament_size', 2, 6)
     offspring_size = round(trial.suggest_float('offspring_size', 0.05, 0.1) * population_size)
-    penalty_weight = trial.suggest_int('penalty_weight', 2000, 10000 ) # higher means more penalty
+    penalty_weight = trial.suggest_int('penalty_weight', 2000, 10000) # higher means more penalty
     # Compute solution and fitness
     _, fitness = ga(iterations, population_size, mutation_rate, tournament_size, offspring_size, penalty_weight)
     df.loc[trial.number] = (fitness, iterations, population_size, mutation_rate, tournament_size, offspring_size, penalty_weight)
